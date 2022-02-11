c5b5591dc04ac2b08565436b123622973102d17b
raulalvesre <raul.alves.re@gmail.com>
2022-02-11 13:13:56 -0300

    refactor: Change Course to not be immutable and change field validation to be done by its setter

--- src/main/java/vayu/models/Course.java
+++ src/main/java/vayu/models/Course.java
@@ -4,68 +4,100 @@ import vayu.utils.TextValidator;
 
 public class Course {
 
-    private final String code;
-    private final String name;
-    private final int timeToFinish;
-    private final boolean isVisible;
-    private final String targetAudience;
-    private final String instructorName;
-    private final String syllabus;
-    private final String developedAbilities;
+    private String code;
+    private String name;
+    private int timeToFinish;
+    private boolean isVisible;
+    private String targetAudience;
+    private String instructorName;
+    private String syllabus;
+    private String developedAbilities;
 
     public Course(String code,
                   String name,
                   int timeToFinish,
-                  boolean isVisible,
-                  String targetAudience,
-                  String instructorName,
-                  String syllabus,
-                  String developedAbilities) {
-        if (!TextValidator.containsOnlyNumbersAndLowerCaseLetters(code))
-            throw new IllegalArgumentException(this.getClass().getSimpleName() + "'s Code should only contain lower case letters, numbers and '-'");
-
-        if (name == null || name.isBlank())
-            throw new IllegalArgumentException(this.getClass().getSimpleName() + "'s Name should not be null or blank");
-
-        this.code = code;
-        this.name = name;
-        this.timeToFinish = timeToFinish;
-        this.isVisible = isVisible;
-        this.targetAudience = targetAudience;
-        this.instructorName = instructorName;
-        this.syllabus = syllabus;
-        this.developedAbilities = developedAbilities;
+                  String instructorName) {
+        setCode(code);
+        setName(name);
+        setTimeToFinish(timeToFinish);
+        isVisible = false;
+        setInstructorName(instructorName);
     }
 
     public String getCode() {
         return code;
     }
 
+    public void setCode(String code) {
+        if (!TextValidator.containsOnlyNumbersAndLowerCaseLetters(code))
+            throw new IllegalArgumentException(this.getClass().getSimpleName() + "'s Code should only contain lower case letters, numbers and '-'");
+
+        this.code = code;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank())
+            throw new IllegalArgumentException(this.getClass().getSimpleName() + "'s Name should not be null or blank");
+
+        this.name = name;
     }
 
     public int getTimeToFinish() {
         return timeToFinish;
     }
 
+    public void setTimeToFinish(int timeToFinish) {
+        if (timeToFinish < 1 || timeToFinish > 20)
+            throw  new IllegalArgumentException(this.getClass().getSimpleName() + "'s time to finish should be between 1 and 20");
+
+        this.timeToFinish = timeToFinish;
+    }
+
     public boolean isVisible() {
         return isVisible;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
     }
 
     public String getTargetAudience() {
         return targetAudience;
     }
 
+    public void setTargetAudience(String targetAudience) {
+        this.targetAudience = targetAudience;
+    }
+
     public String getInstructorName() {
         return instructorName;
+    }
+
+    public void setInstructorName(String instructorName) {
+        if (instructorName.isBlank())
+            throw new IllegalArgumentException(this.getClass().getSimpleName() + "'s instructor's name should not be blank");
+
+        this.instructorName = instructorName;
     }
 
     public String getSyllabus() {
         return syllabus;
     }
 
+    public void setSyllabus(String syllabus) {
+        this.syllabus = syllabus;
+    }
+
     public String getDevelopedAbilities() {
         return developedAbilities;
     }
+
+    public void setDevelopedAbilities(String developedAbilities) {
+        this.developedAbilities = developedAbilities;
+    }
+
 }
