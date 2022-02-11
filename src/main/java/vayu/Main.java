f8fe8dd810c2d989aa29ee7a56b62eeaaf2d7b09
raulalvesre <raul.alves.re@gmail.com>
2022-02-11 14:06:04 -0300

    feat: add constructor with only obligatory fields feat: add getters/setters

--- src/main/java/vayu/Main.java
+++ src/main/java/vayu/Main.java
@@ -5,6 +5,5 @@ import vayu.models.Alternative;
 public class Main {
     public static void main(String[] args) {
         String a = null;
-        Alternative alternative = new Alternative();
     }
 }
--- src/main/java/vayu/models/Alternative.java
+++ src/main/java/vayu/models/Alternative.java
@@ -10,4 +10,53 @@ public class Alternative {
     private String correctnessJustification;
     private Question question;
 
+    public Alternative(String text, boolean isCorrect, Question question) {
+        setText(text);
+        this.isCorrect = isCorrect;
+        this.question = question;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        if (text.isBlank())
+            throw new IllegalArgumentException(this.getClass().getSimpleName() + "'s text should not be blank");
+
+        this.text = text;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+
+    public String getCorrectnessJustification() {
+        return correctnessJustification;
+    }
+
+    public void setCorrectnessJustification(String correctnessJustification) {
+        this.correctnessJustification = correctnessJustification;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
 }
