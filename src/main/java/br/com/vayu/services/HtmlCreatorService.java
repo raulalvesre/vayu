f080eb1c20e81d7087d9b6e3e8030804a593aa22
raulalvesre <raul.alves.re@gmail.com>
2022-02-22 09:22:40 -0300

    refactor: extract methods

--- src/main/java/br/com/vayu/services/HtmlCreatorService.java
+++ src/main/java/br/com/vayu/services/HtmlCreatorService.java
@@ -21,10 +21,6 @@ public class HtmlCreatorService {
     public static void generateCategoriesHtml(Collection<Category> categories,
                                               Collection<SubCategory> subCategories,
                                               Collection<Course> courses) throws URISyntaxException, IOException {
-        List<Category> categoriesListOrdered = categories.stream()
-                .sorted(Comparator.comparingInt(Category::getOrder))
-                .toList();
-
         List<SubCategory> subCategoriesListOrdered = subCategories.stream()
                 .sorted(Comparator.comparingInt(SubCategory::getOrder))
                 .toList();
@@ -34,40 +30,33 @@ public class HtmlCreatorService {
 
         StringBuilder newHtmlBodyContent = new StringBuilder();
 
-        for (Category category : categoriesListOrdered) {
-            long numberOfCoursesInCategory = courses.stream()
-                    .filter(course -> course.getSubCategory().getCategory().equals(category))
-                    .count();
+        categories.stream()
+                .sorted(Comparator.comparingInt(Category::getOrder))
+                .forEachOrdered(category -> {
+                    long numberOfCoursesInCategory = getCoursesRegisteredInCategory(courses, category);
+                    long sumOfEstimatedHoursToFinish = getSumOfCoursesEstimatedHoursToFinishInCategory(courses, category);
 
-            long sumOfEstimatedHoursToFinish = courses.stream()
-                    .filter(course -> course.getSubCategory().getCategory().equals(category))
-                    .mapToInt(Course::getEstimatedHoursToFinish)
-                    .sum();
+                    newHtmlBodyContent.append(String.format("<h2>%s</h2>", category.getName()));
+                    newHtmlBodyContent.append(String.format("<p>%s</p>", category.getDescription()));
+                    newHtmlBodyContent.append(String.format("<img src=\"%s\" style=\"max-height: 300px\"></br>",
+                            category.getIconPath()));
+                    newHtmlBodyContent.append(String.format("<span>Cor: <span style=\"background-color: %s\">ㅤㅤ" +
+                            "</span></span>", category.getColorCode()));
+                    newHtmlBodyContent.append(String.format("<p>Total de cursos: %s", numberOfCoursesInCategory));
+                    newHtmlBodyContent.append(String.format("<p>Total de horas: %s", sumOfEstimatedHoursToFinish));
+                    newHtmlBodyContent.append("<h3>Subcategorias</h3>");
 
-            newHtmlBodyContent.append(String.format("<h2>%s</h2>", category.getName()));
-            newHtmlBodyContent.append(String.format("<p>%s</p>", category.getDescription()));
-            newHtmlBodyContent.append(String.format("<img src=\"%s\" style=\"max-height: 300px\"></br>",
-                    category.getIconPath()));
-            newHtmlBodyContent.append(String.format("<span>Cor: <span style=\"background-color: %s\">ㅤㅤ</span></span>",
-                    category.getColorCode()));
-            newHtmlBodyContent.append(String.format("<p>Total de cursos: %s", numberOfCoursesInCategory));
-            newHtmlBodyContent.append(String.format("<p>Total de horas: %s", sumOfEstimatedHoursToFinish));
-            newHtmlBodyContent.append("<h3>Subcategorias</h3>");
+                    subCategoriesListOrdered.stream()
+                            .filter(sb -> sb.isActive() && sb.getCategory().equals(category))
+                            .forEachOrdered(sb -> {
+                                List<String> namesOfCoursesInThisSubcategory = getNamesOfCoursesInSubcategory(courses, sb);
 
-            subCategoriesListOrdered.stream()
-                    .filter(sb -> sb.isActive() && sb.getCategory().equals(category))
-                    .forEachOrdered(sb -> {
-                        List<String> namesOfCoursesInThisSubcategory = courses.stream()
-                                .filter(course -> course.getSubCategory().equals(sb))
-                                .map(Course::getName)
-                                .toList();
-
-                        newHtmlBodyContent.append(String.format("<h4>%s</h4>", sb.getName()));
-                        newHtmlBodyContent.append(String.format("<p>%s</p>", sb.getDescription()));
-                        newHtmlBodyContent.append("<h5>Cursos</h5>");
-                        newHtmlBodyContent.append(String.join(", ", namesOfCoursesInThisSubcategory));
-                    });
-        }
+                                newHtmlBodyContent.append(String.format("<h4>%s</h4>", sb.getName()));
+                                newHtmlBodyContent.append(String.format("<p>%s</p>", sb.getDescription()));
+                                newHtmlBodyContent.append("<h5>Cursos</h5>");
+                                newHtmlBodyContent.append(String.join(", ", namesOfCoursesInThisSubcategory));
+                            });
+                });
 
         String newHtmlContent = htmlTemplateContent.replace("$body", newHtmlBodyContent);
         Path newHtmlPath = Paths.get(htmlTemplatePath.getParent() +
@@ -80,6 +69,26 @@ public class HtmlCreatorService {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         URL htmlTemplateURL = classLoader.getResource("template.html");
         return Path.of(htmlTemplateURL.toURI());
+    }
+
+    private static long getCoursesRegisteredInCategory(Collection<Course> courses, Category category) {
+        return courses.stream()
+                .filter(course -> course.getSubCategory().getCategory().equals(category))
+                .count();
+    }
+
+    private static long getSumOfCoursesEstimatedHoursToFinishInCategory(Collection<Course> courses, Category category) {
+        return courses.stream()
+                .filter(course -> course.getSubCategory().getCategory().equals(category))
+                .mapToInt(Course::getEstimatedHoursToFinish)
+                .sum();
+    }
+
+    private static List<String> getNamesOfCoursesInSubcategory(Collection<Course> courses, SubCategory subCategory) {
+        return courses.stream()
+                .filter(course -> course.getSubCategory().equals(subCategory))
+                .map(Course::getName)
+                .toList();
     }
 
 }
