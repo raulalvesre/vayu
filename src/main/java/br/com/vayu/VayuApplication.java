de3a4c069ade134c5969ce750186f04e161f0418
raulalvesre <raul.alves.re@gmail.com>
2022-03-31 11:58:42 -0300

    missing files

---
+++ src/main/java/br/com/vayu/VayuApplication.java
@@ -0,0 +1,13 @@
+package br.com.vayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VayuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VayuApplication.class, args);
+	}
+
+}
