2f904828fd14d8d54ccd2b190fc3abc0ab6255bc
raulalvesre <raul.alves.re@gmail.com>
2022-04-13 14:25:55 -0300

    remove unused method

--- src/main/java/br/com/vayu/repositories/CourseRepository.java
+++ src/main/java/br/com/vayu/repositories/CourseRepository.java
@@ -3,8 +3,6 @@ package br.com.vayu.repositories;
 import br.com.vayu.models.Course;
 import br.com.vayu.projections.CourseMinifiedProjection;
 import br.com.vayu.projections.DashboardInstructorProjection;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -23,21 +21,7 @@ public interface CourseRepository extends JpaRepository<Course, Integer> {
             LIMIT 1;""",
             nativeQuery = true)
     DashboardInstructorProjection getDashboardInstructorView();
-
-    @Query(value = """
-                SELECT crs.name, crs.code, crs.visible
-                FROM course crs
-                    INNER JOIN subcategory sb ON crs.subcategory_id = sb.id
-                WHERE sb.code = :sbCode""",
-            countQuery = """
-                        SELECT count(*)
-                        FROM course crs
-                            INNER JOIN subcategory sb ON crs.subcategory_id = sb.id
-                        WHERE sb.code = :sbCode""",
-            nativeQuery = true)
-    Page<CourseMinifiedProjection> findAllMinifiedBySubcategoryCode(@Param("sbCode") String subcategoryCode,
-                                                                    Pageable pageable);
-
+    
     @Query(value = """
                     SELECT crs.name, crs.code, crs.visible
                     FROM course crs
